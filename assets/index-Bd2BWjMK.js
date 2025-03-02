52e6e9c9b4212bfcebe86bb56e0ae5317e5f2e38
ohgus <anakin1108@naver.com>
2025-03-03 01:14:07 +0900

    Updates

--- assets/index-Bd2BWjMK.js
+++ assets/index-Bd2BWjMK.js
@@ -392,7 +392,7 @@ const resetGame = () => {
 const createWinningResultContent = (winningCount, yieldRate) => {
   return `<dialog class="winning-result-dialog" open>
         <button class="winning-result-close-button">
-          <img src="../public/Vector.png" alt="닫기" />
+          <img src="/public/Vector.png" alt="닫기" />
         </button>
         <h3 class="winning-result-title">🏆 당첨 통계 🏆</h3>
         <div class="winning-result-table-container">
